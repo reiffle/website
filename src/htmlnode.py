602243c0de4c847e79cb956c81fb6477c89f8a18
reiffle <pederreiff@gmail.com>
2025-02-01 02:06:40 -0600

    02. Added HTML class

---
+++ src/htmlnode.py
@@ -0,0 +1,21 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag=tag
+        self.value=value
+        self.children=children
+        self.props=props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self):
+        starter=""
+        for value in self.props:
+            starter+=f' {value}="{self.props[value]}"'
+        return starter
+    
+    def __repr__(self):
+        return f"HTMLNode:({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+
