c24861e6731cc667bfd7b38a4f0e182b98ddff82
reiffle <pederreiff@gmail.com>
2025-01-31 21:36:44 -0600

    01: Node test cases established

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+t_type=TextType.BOLD
+def main():
+    test_node= TextNode("My Name Is Lucca", t_type, "http://www.wtf.com")
+    print (test_node)
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq2(self):
+        node = TextNode("This is a text node", TextType.BOLD, "http://www.ign.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "http://www.ign.com")
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        node2 = TextNode("This is a text node", TextType.BOLD, "http://www.ign.com")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq2(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq3(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node too", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
