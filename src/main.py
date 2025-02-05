f222450cf281457826fe1248cf062840e81dac71
reiffle <pederreiff@gmail.com>
2025-02-05 17:55:37 -0600

    06. Extraction Markdown added

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from textnode import *
+from extract_markdown import *
 
 t_type=TextType.BOLD
 def main():
     test_node= TextNode("My Name Is Lucca", t_type, "http://www.wtf.com")
     print (test_node)
+
 
 main()
