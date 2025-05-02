702e4a376142608bb1b8b0834183bdaa382154ec
reiffle <pederreiff@gmail.com>
2025-05-01 22:29:23 -0500

    Fixed problem with sys

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import sys
 from copy_files import copy_from_source_to_target, generate_pages_recursive
 def main():
-    if len(sys.argv)==0:
-        basepath = "/"
-    else: basepath = sys.argv[0]
+    basepath = "/"
+    if len(sys.argv)>1:
+        basepath = sys.argv[1]
     home_dir="/home/pederreiff/workspace/github.com/reiffle/website"
     copy_from_source_to_target()
     generate_pages_recursive(basepath, f"{home_dir}/content", f"{home_dir}/template.html", f"{home_dir}/docs/")
